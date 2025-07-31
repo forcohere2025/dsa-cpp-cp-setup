9e52e0b7e6a19d1ebe315507918754cbb22b528d
forcohere2025 <forcohere2025@hotmail.com>
2025-07-31 17:52:19 +0000

    Updated tasks.json

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    cout << "Enter your name: ";
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+
     return 0;
 }
